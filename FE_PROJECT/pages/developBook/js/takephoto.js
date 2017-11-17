0b3cabf97aa01796541a759925e8a962cbc877d9
patrickxy <18200569466@163.com>
2017-11-17 11:29:23 +0800

    新增cssTest文件夹
    cssTest 用于实现各种css效果测试等
    包含1.css揭秘文档等

--- FE_PROJECT/pages/developBook/js/takephoto.js
+++ FE_PROJECT/pages/developBook/js/takephoto.js
@@ -10,21 +10,43 @@ $(function() {
     // 摇一摇功能
     // 访问传感器的API 
     // 方向事件 deviceorientation
-    window.addEventListener('deviceorientation', orientationHandle, true);
+    // window.addEventListener('deviceorientation', orientationHandle, true);
 
-    function orientationHandle(evt) {
-        //console.log(evt);/
-    }
+    // function orientationHandle(evt) {
+    //console.log(evt);/
+    // }
     // 移动事件 devicemotion 
     if (window.DeviceMotionEvent) {
         window.addEventListener('devicemotion', motionHandle, false);
     }
+    var SHARE_SPEED_THRESHOLD = 300; //摇动速度阈值
+    var lastTime = 0; //上次变化的时间
 
+    let x = 0,
+        y = 0,
+        z = 0,
+        lastX = 0,
+        lastY = 0,
+        lastZ = 0;
+    console.log(test1);
 
     function motionHandle(event) {
         var acceleration = event.accelerationIncludingGravity;
-        console.log(acceleration.x);
-        console.log(acceleration.y);
-        console.log(acceleration.z);
+
+        let nowTime = new Date();
+        if ((nowTime - lastTime) > 120) { //大于120ms
+            let diffTime = nowTime - lastTime; //时间差 用于求速度
+            lastTime = nowTime;
+            x = acceleration.x;
+            y = acceleration.y;
+            z = acceleration.z;
+            let speed = Math.abs(x + y + z - lastX - lastY - lastZ) / diffTime * 1000; //1s的速度
+            if (speed > SHARE_SPEED_THRESHOLD) {
+                alert('您摇动了手机！')
+            }
+            lastX = x;
+            lastY = y;
+            lastZ = z;
+        }
     }
 });
